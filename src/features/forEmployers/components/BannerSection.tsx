1feed1bb09ef63b9c9969781fe040a3ab8855e39
ahmedehab72 <ahmedehab.eng@gmail.com>
2026-04-27 14:15:18 +0300

    fix: update BannerSection component styles and image properties

--- src/features/forEmployers/components/BannerSection.tsx
+++ src/features/forEmployers/components/BannerSection.tsx
@@ -20,7 +20,7 @@ export default function BannerSection({
     <section className="relative bg-white ">
       <div className="mx-auto flex flex-col items-center gap-6 rounded-[50px] bg-primary-gradient px-4 sm:px-6 lg:flex-row md:gap-10 lg:py-4 lg:gap-16 lg:px-0">
         {/* <div className="absolute top-1/2 -left-20 h-11.5 w-75 -translate-y-1/2 rotate-22 rounded-full bg-primary-bg opacity-80 blur-[200px] sm:-left-40 sm:w-[503px]" /> */}
-        <div className="absolute top-1/2 -right-20 h-11.5 w-75 -translate-y-1/2 rotate-22 rounded-full  opacity-80 blur-[200px] sm:-right-40 sm:w-[503px]" />
+        {/* <div className="absolute top-1/2 -right-20 h-11.5 w-75 -translate-y-1/2 rotate-22 rounded-full  opacity-80 blur-[200px] sm:-right-40 sm:w-[503px]" /> */}
 
         <div className="px-4 pt-8 text-center text-white sm:px-6 md:px-16">
           <h2 className="mb-4 text-2xl leading-tight font-bold text-white sm:text-4xl lg:text-5xl">
@@ -46,12 +46,13 @@ export default function BannerSection({
           </Link>
         </div>
 
-        <div className="mt-6 w-full rounded-2xl md:mt-0">
+        <div className="w-full rounded-2xl md:mt-0">
           <Image
             src={bannerImage.image}
             alt={bannerImage.alt}
-            fill
-            className="lg:absolute object-cover bottom-0 md:right-0 h-75 sm:h-95 lg:[600px]"
+            width={500}
+            height={600}
+            className="relative lg:absolute object-contain bottom-0 md:right-0 h-75 sm:h-95 lg:[600px]"
           />
         </div>
       </div>
